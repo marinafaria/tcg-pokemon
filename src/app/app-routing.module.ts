b608918312e1395d24849f52d25b9ed0081c3642
Marina <maarina.faria@gmail.com>
2024-01-03 18:14:19 -0300

    build: initial config

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', 
+    loadChildren: async () => (await import('./pages/home/home.module')).HomeModule
+  },
+  {
+    path: '**', 
+    redirectTo: ''
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {        
+      spacing: {
+        '72': '18rem',
+        '84': '21rem',
+        '96': '24rem',
+        '108': '27rem'
+      }
+    },
+  },
+  plugins: [],
+}
+
